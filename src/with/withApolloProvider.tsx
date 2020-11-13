f506b85e183ee018c33d5d092d2636102221f926
Michal Kára <mk250798@ncr.com>
2020-11-13 22:03:38 +0100

    - GraphQL URL is determined from request

--- src/with/withApolloProvider.tsx
+++ src/with/withApolloProvider.tsx
@@ -9,9 +9,21 @@ if (!process.browser) {
 
 export const withApolloWrapper = withApollo(props => {
     const { initialState } = props
+    let uri = 'http://localhost:3000/graphql' // Fallback
+
+    if (global.window) {
+        // Do requests back to origin
+        uri = `${global.window.location.origin}/graphql`
+    } else if (props.headers?.host) {
+        // Server - get host from headers
+        const host = props.headers?.host || ''
+        const protocol = host.indexOf('localhost') >= 0 ? 'http://' : 'https://'
+        uri = `${protocol}${host}/graphql`
+    }
+
     return new ApolloClient({
         link: createHttpLink({
-            uri: 'http://localhost:3000/graphql',
+            uri,
             credentials: 'same-origin',
         }),
         cache: new InMemoryCache().restore(initialState || {}),
