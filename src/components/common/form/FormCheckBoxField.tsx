ce009bc442fdd7a3845e2e2496177acbcf7699a1
Michal Kára <mk250798@ncr.com>
2020-12-28 14:37:58 +0100

    Refactor some form fields - simplify field & its wrapper into one component

--- src/components/common/form/FormCheckBoxField.tsx
+++ src/components/common/form/FormCheckBoxField.tsx
@@ -1,19 +1,40 @@
 import React from 'react'
-import { Field } from 'react-final-form'
-import FormCheckBox, { FormCheckBoxProps } from './FormCheckBox'
+import { useField } from 'react-final-form'
+import { Form } from 'react-bootstrap'
+import FieldWithError from './FieldWithError'
 
-export interface FormCheckBoxFieldProps extends Omit<FormCheckBoxProps, 'input' | 'meta'> {
+export interface FormCheckBoxFieldProps {
     readonly name: string
+    readonly showErrorPlaceholder?: boolean
+    readonly label: string
+    readonly hint?: string
 }
 
 /**
  * Convenience wrapper around FormCheckBox
  */
-const FormCheckBoxField = ({ name, ...inputProps }: FormCheckBoxFieldProps) => (
-    <Field<boolean> name={name}>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {({ input, meta }) => <FormCheckBox {...inputProps} input={input} meta={meta} />}
-    </Field>
-)
+const FormCheckBoxField = ({ name, label, hint, showErrorPlaceholder }: FormCheckBoxFieldProps) => {
+    const {
+        input: { value, ...inputRest },
+        meta,
+    } = useField<boolean>(name)
+
+    return (
+        <FieldWithError meta={meta} hint={hint} showErrorPlaceholder={showErrorPlaceholder}>
+            {isInvalid => (
+                <>
+                    <Form.Check
+                        isInvalid={isInvalid}
+                        type={'checkbox' as any}
+                        label={label}
+                        checked={value}
+                        /* eslint-disable-next-line react/jsx-props-no-spreading */
+                        {...inputRest}
+                    />
+                </>
+            )}
+        </FieldWithError>
+    )
+}
 
 export default FormCheckBoxField
--- src/components/common/form/FormTextInputField.tsx
+++ src/components/common/form/FormTextInputField.tsx
@@ -1,22 +1,100 @@
-import React from 'react'
-import { Field } from 'react-final-form'
+import React, { ChangeEvent, FocusEvent, useState } from 'react'
+import { useField } from 'react-final-form'
 import { FieldValidator } from 'final-form'
-import FormTextInput, { FormTextInputProps } from './FormTextInput'
+import { Form, InputGroup } from 'react-bootstrap'
+import FieldWithError from './FieldWithError'
 
-export interface FormTextInputFieldProps extends Omit<FormTextInputProps, 'input' | 'meta'> {
+export interface FormTextInputFieldProps {
     readonly name: string
     readonly hint?: string
     readonly validate?: FieldValidator<string>
+    readonly showErrorPlaceholder?: boolean
+    readonly label?: string
+    readonly placeholder?: string
+    readonly appendIcon?: React.ReactElement
+    readonly type?: string
+    readonly onBlur?: (e: FocusEvent<HTMLInputElement>) => void
+    readonly onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+    readonly errorHint?: React.ReactNode
+    readonly autoFocus?: boolean
 }
 
 /**
  * Convenience wrapper around FormTextInput
  */
-const FormTextInputField = ({ name, hint, validate, ...inputProps }: FormTextInputFieldProps) => (
-    <Field<string> name={name} validate={validate}>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {({ input, meta }) => <FormTextInput {...inputProps} hint={hint} input={input} meta={meta} />}
-    </Field>
-)
+const FormTextInputField = ({
+    name,
+    hint,
+    validate,
+    showErrorPlaceholder = true,
+    placeholder,
+    label,
+    type,
+    appendIcon,
+    errorHint,
+    autoFocus,
+    onBlur,
+    onChange,
+}: FormTextInputFieldProps) => {
+    const {
+        input: { onBlur: inputOnBlur, onChange: inputOnChange, ...inputRest },
+        meta,
+    } = useField(name, { validate })
+    const [wasBlurred, setWasBlurred] = useState(false)
+
+    const handleOnBlur = (e: FocusEvent<HTMLInputElement>) => {
+        setWasBlurred(true)
+        inputOnBlur?.(e)
+        onBlur?.(e)
+    }
+
+    const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+        inputOnChange?.(e)
+        onChange?.(e)
+    }
+
+    return (
+        <FieldWithError
+            meta={meta}
+            showErrorPlaceholder={showErrorPlaceholder}
+            hint={hint}
+            errorHint={errorHint}
+            hideError={!wasBlurred}
+        >
+            {isInvalid => (
+                <>
+                    {label && <Form.Label>{label}</Form.Label>}
+                    {appendIcon && (
+                        <InputGroup>
+                            <Form.Control
+                                isInvalid={isInvalid}
+                                type={type}
+                                onBlur={handleOnBlur}
+                                onChange={handleOnChange}
+                                autoFocus={autoFocus}
+                                /* eslint-disable-next-line react/jsx-props-no-spreading */
+                                {...inputRest}
+                                placeholder={placeholder}
+                            />
+                            <InputGroup.Append>{appendIcon}</InputGroup.Append>
+                        </InputGroup>
+                    )}
+                    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                    {!appendIcon && (
+                        <Form.Control
+                            isInvalid={isInvalid}
+                            type={type}
+                            onBlur={handleOnBlur}
+                            onChange={handleOnChange}
+                            /* eslint-disable-next-line react/jsx-props-no-spreading */
+                            {...inputRest}
+                            placeholder={placeholder}
+                        />
+                    )}
+                </>
+            )}
+        </FieldWithError>
+    )
+}
 
 export default FormTextInputField
